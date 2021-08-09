18529c50e6a912390bc11b7c8d6bdb1a56ab3fca
dbtrnl <daniel.bittar@alumni.usp.br>
2021-08-09 00:45:37 -0300

    tests(typescripts): refactor tests names

--- typescript/src/problems/11-20/11-20.test.ts
+++ typescript/src/problems/11-20/11-20.test.ts
@@ -4,23 +4,23 @@ import {
 } from '.'
 
 describe('Problems 11 to 20', () => {
-  it('problem 11', () => { const result = problem11(); expect(result).to.be.equal(answers[0]) })
+  it('problem11', () => { const result = problem11(); expect(result).to.be.equal(answers[0]) })
 
-  it('problem 12', () => { const result = problem12(); expect(result).to.be.equal(answers[1]) })
+  it('problem12', () => { const result = problem12(); expect(result).to.be.equal(answers[1]) })
 
-  it('problem 13', () => { const result = problem13(); expect(result).to.be.equal(answers[2]) })
+  it('problem13', () => { const result = problem13(); expect(result).to.be.equal(answers[2]) })
 
-  it('problem 14', () => { const result = problem14(); expect(result).to.be.equal(answers[3]) })
+  it('problem14', () => { const result = problem14(); expect(result).to.be.equal(answers[3]) })
 
-  it('problem 15', () => { const result = problem15(); expect(result).to.be.equal(answers[4]) })
+  it('problem15', () => { const result = problem15(); expect(result).to.be.equal(answers[4]) })
 
-  it('problem 16', () => { const result = problem16(); expect(result).to.be.equal(answers[5]) })
+  it('problem16', () => { const result = problem16(); expect(result).to.be.equal(answers[5]) })
 
-  it('problem 17', () => { const result = problem17(); expect(result).to.be.equal(answers[6]) })
+  it('problem17', () => { const result = problem17(); expect(result).to.be.equal(answers[6]) })
 
-  it('problem 18', () => { const result = problem18(); expect(result).to.be.equal(answers[7]) })
+  it('problem18', () => { const result = problem18(); expect(result).to.be.equal(answers[7]) })
 
-  it('problem 19', () => { const result = problem19(); expect(result).to.be.equal(answers[8]) })
+  it('problem19', () => { const result = problem19(); expect(result).to.be.equal(answers[8]) })
 
-  it('problem 20', () => { const result = problem20(); expect(result).to.be.equal(answers[9]) })
+  it('problem20', () => { const result = problem20(); expect(result).to.be.equal(answers[9]) })
 })

d779153a958f95c9a70a639f1e3958140743cad5
dbtrnl <daniel.bittar@alumni.usp.br>
2021-08-11 01:22:27 -0300

    tests(typescript/utils): more tests on divisorsProductsUtils
    func isEvenlyDivisibleByEveryNumberInInterval() needs fixing

--- typescript/src/utils/divisorsProductsUtils.test.ts
+++ typescript/src/utils/divisorsProductsUtils.test.ts
@@ -4,8 +4,12 @@ import { findAbundantNumbersUntil } from '.'
 import {
   findAllDivisors,
   findAllProperDivisors,
+  findAndSumAllDivisors,
+  findAndSumAllProperDivisors,
   isAmicableNumber,
+  isEvenlyDivisibleByEveryNumberInInterval,
   isNumberDeficientPerfectOrAbundant,
+  isNumberEvenlyDivisibleBy,
 } from './divisorsProductsUtils'
 
 describe('Test divisorsProductsUtils', () => {
@@ -63,9 +67,218 @@ describe('Test divisorsProductsUtils', () => {
   })
 
   describe('findAllDivisors()', () => {
+    it('should return NULL when calling function with (0)', () => {
+      const expected: AllDivisors = null
+      const result = findAllDivisors(0); expect(result).to.be.equal(expected)
+    })
+
+    it('should return expected result when calling function with (1)', () => {
+      const expected: AllDivisors = [1]
+      const result = findAllDivisors(1); expect(result).to.have.members(expected)
+    })
+
+    it('should return expected result when calling function with (2)', () => {
+      const expected: AllDivisors = [1, 2]
+      const result = findAllDivisors(2); expect(result).to.have.members(expected)
+    })
+
+    it('should return expected result when calling function with (8)', () => {
+      const expected: AllDivisors = [1, 2, 4, 8]
+      const result = findAllDivisors(8); expect(result).to.have.members(expected)
+    })
+
+    it('should return expected result when calling function with (15)', () => {
+      const expected: AllDivisors = [1, 3, 5, 15]
+      const result = findAllDivisors(15); expect(result).to.have.members(expected)
+    })
+
+    it('should return expected result when calling function with (16)', () => {
+      const expected: AllDivisors = [1, 2, 4, 8, 16]
+      const result = findAllDivisors(16); expect(result).to.have.members(expected)
+    })
+
+    it('should return expected result when calling function with (100)', () => {
+      const expected: AllDivisors = [1, 2, 4, 5, 10, 20, 25, 50, 100]
+      const result = findAllDivisors(100); expect(result).to.have.members(expected)
+    })
+
+    it('should return expected result when calling function with (1000)', () => {
+      const expected: AllDivisors = [1, 2, 4, 5, 8, 10, 20, 25, 40, 50, 100, 125, 200, 250, 500, 1000]
+      const result = findAllDivisors(1000); expect(result).to.have.members(expected)
+    })
+
+    it('should return expected result when calling function with (1001)', () => {
+      const expected: AllDivisors = [1, 7, 11, 13, 77, 91, 143, 1001]
+      const result = findAllDivisors(1001); expect(result).to.have.members(expected)
+    })
+
     it('should return expected result when calling function with (1003)', () => {
       const expected: AllDivisors = [1, 17, 59, 1003]
       const result = findAllDivisors(1003); expect(result).to.have.members(expected)
+    })
+  })
+
+  describe('findAndSumAllProperDivisors', () => {
+    it('should return NULL when calling function with (0)', () => {
+      const expected: AllDivisors = null
+      const result = findAndSumAllProperDivisors(0); expect(result).to.be.equal(expected)
+    })
+
+    it('should return expected result when calling function with (1)', () => {
+      const expected = 1
+      const result = findAndSumAllProperDivisors(1); expect(result).to.be.equal(expected)
+    })
+
+    it('should return expected result when calling function with (2)', () => {
+      const expected = 1
+      const result = findAndSumAllProperDivisors(2); expect(result).to.be.equal(expected)
+    })
+
+    it('should return expected result when calling function with (8)', () => {
+      const expected = 7
+      const result = findAndSumAllProperDivisors(8); expect(result).to.be.equal(expected)
+    })
+
+    it('should return expected result when calling function with (15)', () => {
+      const expected = 9
+      const result = findAndSumAllProperDivisors(15); expect(result).to.be.equal(expected)
+    })
+
+    it('should return expected result when calling function with (16)', () => {
+      const expected = 15
+      const result = findAndSumAllProperDivisors(16); expect(result).to.be.equal(expected)
+    })
+
+    it('should return expected result when calling function with (100)', () => {
+      const expected = 117
+      const result = findAndSumAllProperDivisors(100); expect(result).to.be.equal(expected)
+    })
+
+    it('should return expected result when calling function with (1000)', () => {
+      const expected = 1340
+      const result = findAndSumAllProperDivisors(1000); expect(result).to.be.equal(expected)
+    })
+
+    it('should return expected result when calling function with (1001)', () => {
+      const expected = 343
+      const result = findAndSumAllProperDivisors(1001); expect(result).to.be.equal(expected)
+    })
+
+    it('should return expected result when calling function with (1003)', () => {
+      const expected = 77
+      const result = findAndSumAllProperDivisors(1003); expect(result).to.be.equal(expected)
+    })
+  })
+
+  describe('findAndSumAllDivisors', () => {
+    it('should return NULL when calling function with (0)', () => {
+      const expected: AllDivisors = null
+      const result = findAndSumAllDivisors(0); expect(result).to.be.equal(expected)
+    })
+
+    it('should return expected result when calling function with (1)', () => {
+      const expected = 1
+      const result = findAndSumAllDivisors(1); expect(result).to.be.equal(expected)
+    })
+
+    it('should return expected result when calling function with (2)', () => {
+      const expected = 3
+      const result = findAndSumAllDivisors(2); expect(result).to.be.equal(expected)
+    })
+
+    it('should return expected result when calling function with (8)', () => {
+      const expected = 15
+      const result = findAndSumAllDivisors(8); expect(result).to.be.equal(expected)
+    })
+
+    it('should return expected result when calling function with (15)', () => {
+      const expected = 24
+      const result = findAndSumAllDivisors(15); expect(result).to.be.equal(expected)
+    })
+
+    it('should return expected result when calling function with (16)', () => {
+      const expected = 31
+      const result = findAndSumAllDivisors(16); expect(result).to.be.equal(expected)
+    })
+
+    it('should return expected result when calling function with (100)', () => {
+      const expected = 217
+      const result = findAndSumAllDivisors(100); expect(result).to.be.equal(expected)
+    })
+
+    it('should return expected result when calling function with (1000)', () => {
+      const expected = 2340
+      const result = findAndSumAllDivisors(1000); expect(result).to.be.equal(expected)
+    })
+
+    it('should return expected result when calling function with (1001)', () => {
+      const expected = 1344
+      const result = findAndSumAllDivisors(1001); expect(result).to.be.equal(expected)
+    })
+
+    it('should return expected result when calling function with (1003)', () => {
+      const expected = 1080
+      const result = findAndSumAllDivisors(1003); expect(result).to.be.equal(expected)
+    })
+  })
+
+  describe('isNumberEvenlyDivisibleBy()', () => {
+    it('should throw RangeError when calling function with zero and/or negative values', () => {
+      expect(() => isNumberEvenlyDivisibleBy(-238293, -23234)).to.throw(
+        RangeError, 'Both arguments must be greater than zero!',
+      )
+      expect(() => isNumberEvenlyDivisibleBy(-1, 0)).to.throw(
+        RangeError, 'Both arguments must be greater than zero!',
+      )
+      expect(() => isNumberEvenlyDivisibleBy(0, -1)).to.throw(
+        RangeError, 'Both arguments must be greater than zero!',
+      )
+      expect(() => isNumberEvenlyDivisibleBy(0, 0)).to.throw(
+        RangeError, 'Both arguments must be greater than zero!',
+      )
+    })
+
+    it('should return TRUE result when calling function with (1, 1)', () => {
+      const result = isNumberEvenlyDivisibleBy(2, 2); expect(result).to.be.equal(true)
+    })
+
+    it('should return TRUE result when calling function with (2, 2)', () => {
+      const result = isNumberEvenlyDivisibleBy(2, 2); expect(result).to.be.equal(true)
+    })
+
+    it('should return TRUE result when calling function with (200, 4)', () => {
+      const result = isNumberEvenlyDivisibleBy(200, 4); expect(result).to.be.equal(true)
+    })
+
+    it('should return FALSE result when calling function with (4, 200)', () => {
+      const result = isNumberEvenlyDivisibleBy(4, 200); expect(result).to.be.equal(false)
+    })
+
+    it('should return FALSE result when calling function with (2, 4)', () => {
+      const result = isNumberEvenlyDivisibleBy(2, 4); expect(result).to.be.equal(false)
+    })
+  })
+
+  describe('isEvenlyDivisibleByEveryNumberInInterval()', () => {
+    it('should throw RangeError when calling function with invalid interval', () => {
+      expect(() => isEvenlyDivisibleByEveryNumberInInterval(100, [2, 3], 'ascending')).to.throw(
+        RangeError, 'Both arguments must be greater than zero!',
+      )
+      expect(() => isEvenlyDivisibleByEveryNumberInInterval(100, [0, 2323], 'descending')).to.throw(
+        RangeError, 'Both arguments must be greater than zero!',
+      )
+    })
+
+    it('should return TRUE when calling function with (100, [1, 2], "ascending")', () => {
+      const result = isEvenlyDivisibleByEveryNumberInInterval(100, [1, 2], 'ascending'); expect(result).to.be.equal(true)
+    })
+
+    it('should return FALSE when calling function with (100, [1, 4], "ascending")', () => {
+      const result = isEvenlyDivisibleByEveryNumberInInterval(100, [1, 4], 'ascending'); expect(result).to.be.equal(true)
+    })
+
+    it('should return FALSE when calling function with (1432, [1, 1432], "descending")', () => {
+      const result = isEvenlyDivisibleByEveryNumberInInterval(1432, [1, 1432], 'descending'); expect(result).to.be.equal(true)
     })
   })
 

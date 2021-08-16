01636559b8284a2979e2b5cdc0934fc55036020a
dbtrnl <daniel.bittar@alumni.usp.br>
2021-08-16 01:02:56 -0300

    tests(typescript) add Istambul (nyc) for test coverage
    also refactoring some test descriptions
    also, problem5 is timing out when using coverage, need to investigate

--- typescript/src/utils/divisorsProductsUtils.test.ts
+++ typescript/src/utils/divisorsProductsUtils.test.ts
@@ -25,47 +25,47 @@ describe('Test divisorsProductsUtils', () => {
       const result = findAllProperDivisors(0); expect(result).to.be.deep.equal(expected)
     })
 
-    it('should return expected result when calling function with (1)', () => {
+    it('should return expected array when calling function with (1)', () => {
       const expected: AllDivisors = [1]
       const result = findAllProperDivisors(1); expect(result).to.have.members(expected)
     })
 
-    it('should return expected result when calling function with (2)', () => {
+    it('should return expected array when calling function with (2)', () => {
       const expected: AllDivisors = [1]
       const result = findAllProperDivisors(2); expect(result).to.have.members(expected)
     })
 
-    it('should return expected result when calling function with (8)', () => {
+    it('should return expected array when calling function with (8)', () => {
       const expected: AllDivisors = [1, 2, 4]
       const result = findAllProperDivisors(8); expect(result).to.have.members(expected)
     })
 
-    it('should return expected result when calling function with (15)', () => {
+    it('should return expected array when calling function with (15)', () => {
       const expected: AllDivisors = [1, 3, 5]
       const result = findAllProperDivisors(15); expect(result).to.have.members(expected)
     })
 
-    it('should return expected result when calling function with (16)', () => {
+    it('should return expected array when calling function with (16)', () => {
       const expected: AllDivisors = [1, 2, 4, 8]
       const result = findAllProperDivisors(16); expect(result).to.have.members(expected)
     })
 
-    it('should return expected result when calling function with (100)', () => {
+    it('should return expected array when calling function with (100)', () => {
       const expected: AllDivisors = [1, 2, 4, 5, 10, 20, 25, 50]
       const result = findAllProperDivisors(100); expect(result).to.have.members(expected)
     })
 
-    it('should return expected result when calling function with (1000)', () => {
+    it('should return expected array when calling function with (1000)', () => {
       const expected: AllDivisors = [1, 2, 4, 5, 8, 10, 20, 25, 40, 50, 100, 125, 200, 250, 500]
       const result = findAllProperDivisors(1000); expect(result).to.have.members(expected)
     })
 
-    it('should return expected result when calling function with (1001)', () => {
+    it('should return expected array when calling function with (1001)', () => {
       const expected: AllDivisors = [1, 7, 11, 13, 77, 91, 143]
       const result = findAllProperDivisors(1001); expect(result).to.have.members(expected)
     })
 
-    it('should return expected result when calling function with (1003)', () => {
+    it('should return expected array when calling function with (1003)', () => {
       const expected: AllDivisors = [1, 17, 59]
       const result = findAllProperDivisors(1003); expect(result).to.have.members(expected)
     })
@@ -77,47 +77,47 @@ describe('Test divisorsProductsUtils', () => {
       const result = findAllDivisors(0); expect(result).to.be.deep.equal(expected)
     })
 
-    it('should return expected result when calling function with (1)', () => {
+    it('should return expected array when calling function with (1)', () => {
       const expected: AllDivisors = [1]
       const result = findAllDivisors(1); expect(result).to.have.members(expected)
     })
 
-    it('should return expected result when calling function with (2)', () => {
+    it('should return expected array when calling function with (2)', () => {
       const expected: AllDivisors = [1, 2]
       const result = findAllDivisors(2); expect(result).to.have.members(expected)
     })
 
-    it('should return expected result when calling function with (8)', () => {
+    it('should return expected array when calling function with (8)', () => {
       const expected: AllDivisors = [1, 2, 4, 8]
       const result = findAllDivisors(8); expect(result).to.have.members(expected)
     })
 
-    it('should return expected result when calling function with (15)', () => {
+    it('should return expected array when calling function with (15)', () => {
       const expected: AllDivisors = [1, 3, 5, 15]
       const result = findAllDivisors(15); expect(result).to.have.members(expected)
     })
 
-    it('should return expected result when calling function with (16)', () => {
+    it('should return expected array when calling function with (16)', () => {
       const expected: AllDivisors = [1, 2, 4, 8, 16]
       const result = findAllDivisors(16); expect(result).to.have.members(expected)
     })
 
-    it('should return expected result when calling function with (100)', () => {
+    it('should return expected array when calling function with (100)', () => {
       const expected: AllDivisors = [1, 2, 4, 5, 10, 20, 25, 50, 100]
       const result = findAllDivisors(100); expect(result).to.have.members(expected)
     })
 
-    it('should return expected result when calling function with (1000)', () => {
+    it('should return expected array when calling function with (1000)', () => {
       const expected: AllDivisors = [1, 2, 4, 5, 8, 10, 20, 25, 40, 50, 100, 125, 200, 250, 500, 1000]
       const result = findAllDivisors(1000); expect(result).to.have.members(expected)
     })
 
-    it('should return expected result when calling function with (1001)', () => {
+    it('should return expected array when calling function with (1001)', () => {
       const expected: AllDivisors = [1, 7, 11, 13, 77, 91, 143, 1001]
       const result = findAllDivisors(1001); expect(result).to.have.members(expected)
     })
 
-    it('should return expected result when calling function with (1003)', () => {
+    it('should return expected array when calling function with (1003)', () => {
       const expected: AllDivisors = [1, 17, 59, 1003]
       const result = findAllDivisors(1003); expect(result).to.have.members(expected)
     })
@@ -129,47 +129,47 @@ describe('Test divisorsProductsUtils', () => {
       const result = findAndSumAllProperDivisors(0); expect(result).to.be.deep.equal(expected)
     })
 
-    it('should return expected result when calling function with (1)', () => {
-      const expected = 1
+    it('should return 0 result when calling function with (1)', () => {
+      const expected = 0
       const result = findAndSumAllProperDivisors(1); expect(result).to.be.equal(expected)
     })
 
-    it('should return expected result when calling function with (2)', () => {
+    it('should return 1 when calling function with (2)', () => {
       const expected = 1
       const result = findAndSumAllProperDivisors(2); expect(result).to.be.equal(expected)
     })
 
-    it('should return expected result when calling function with (8)', () => {
+    it('should return 7 when calling function with (8)', () => {
       const expected = 7
       const result = findAndSumAllProperDivisors(8); expect(result).to.be.equal(expected)
     })
 
-    it('should return expected result when calling function with (15)', () => {
+    it('should return 9 when calling function with (15)', () => {
       const expected = 9
       const result = findAndSumAllProperDivisors(15); expect(result).to.be.equal(expected)
     })
 
-    it('should return expected result when calling function with (16)', () => {
+    it('should return 15 when calling function with (16)', () => {
       const expected = 15
       const result = findAndSumAllProperDivisors(16); expect(result).to.be.equal(expected)
     })
 
-    it('should return expected result when calling function with (100)', () => {
+    it('should return 117 when calling function with (100)', () => {
       const expected = 117
       const result = findAndSumAllProperDivisors(100); expect(result).to.be.equal(expected)
     })
 
-    it('should return expected result when calling function with (1000)', () => {
+    it('should return 1340 when calling function with (1000)', () => {
       const expected = 1340
       const result = findAndSumAllProperDivisors(1000); expect(result).to.be.equal(expected)
     })
 
-    it('should return expected result when calling function with (1001)', () => {
+    it('should return 343 when calling function with (1001)', () => {
       const expected = 343
       const result = findAndSumAllProperDivisors(1001); expect(result).to.be.equal(expected)
     })
 
-    it('should return expected result when calling function with (1003)', () => {
+    it('should return 77 when calling function with (1003)', () => {
       const expected = 77
       const result = findAndSumAllProperDivisors(1003); expect(result).to.be.equal(expected)
     })
@@ -181,47 +181,47 @@ describe('Test divisorsProductsUtils', () => {
       const result = findAndSumAllDivisors(0); expect(result).to.be.deep.equal(expected)
     })
 
-    it('should return expected result when calling function with (1)', () => {
+    it('should return 1 when calling function with (1)', () => {
       const expected = 1
       const result = findAndSumAllDivisors(1); expect(result).to.be.equal(expected)
     })
 
-    it('should return expected result when calling function with (2)', () => {
+    it('should return 3 when calling function with (2)', () => {
       const expected = 3
       const result = findAndSumAllDivisors(2); expect(result).to.be.equal(expected)
     })
 
-    it('should return expected result when calling function with (8)', () => {
+    it('should return 15 when calling function with (8)', () => {
       const expected = 15
       const result = findAndSumAllDivisors(8); expect(result).to.be.equal(expected)
     })
 
-    it('should return expected result when calling function with (15)', () => {
+    it('should return 24 when calling function with (15)', () => {
       const expected = 24
       const result = findAndSumAllDivisors(15); expect(result).to.be.equal(expected)
     })
 
-    it('should return expected result when calling function with (16)', () => {
+    it('should return 31 when calling function with (16)', () => {
       const expected = 31
       const result = findAndSumAllDivisors(16); expect(result).to.be.equal(expected)
     })
 
-    it('should return expected result when calling function with (100)', () => {
+    it('should return 217 when calling function with (100)', () => {
       const expected = 217
       const result = findAndSumAllDivisors(100); expect(result).to.be.equal(expected)
     })
 
-    it('should return expected result when calling function with (1000)', () => {
+    it('should return 2340 when calling function with (1000)', () => {
       const expected = 2340
       const result = findAndSumAllDivisors(1000); expect(result).to.be.equal(expected)
     })
 
-    it('should return expected result when calling function with (1001)', () => {
+    it('should return 1344 when calling function with (1001)', () => {
       const expected = 1344
       const result = findAndSumAllDivisors(1001); expect(result).to.be.equal(expected)
     })
 
-    it('should return expected result when calling function with (1003)', () => {
+    it('should return 1080 when calling function with (1003)', () => {
       const expected = 1080
       const result = findAndSumAllDivisors(1003); expect(result).to.be.equal(expected)
     })
@@ -303,27 +303,27 @@ describe('Test divisorsProductsUtils', () => {
       )
     })
 
-    it('should return expected result when calling function with (220)', () => {
+    it('should return expected object when calling function with (220)', () => {
       const expected: AmicableNumberObject = { isAmicable: true, pair: [220, 284] }
       const result = isAmicableNumber(220); expect(result).to.be.deep.equal(expected)
     })
 
-    it('should return expected result when calling function with (2620)', () => {
+    it('should return expected object when calling function with (2620)', () => {
       const expected: AmicableNumberObject = { isAmicable: true, pair: [2620, 2924] }
       const result = isAmicableNumber(2620); expect(result).to.be.deep.equal(expected)
     })
 
-    it('should return expected result when calling function with (79750)', () => {
+    it('should return expected object when calling function with (79750)', () => {
       const expected: AmicableNumberObject = { isAmicable: true, pair: [79750, 88730] }
       const result = isAmicableNumber(79750); expect(result).to.be.deep.equal(expected)
     })
 
-    it('should return expected result when calling function with (221)', () => {
+    it('should return expected object when calling function with (221)', () => {
       const expected: AmicableNumberObject = { isAmicable: false, pair: null }
       const result = isAmicableNumber(221); expect(result).to.be.deep.equal(expected)
     })
 
-    it('should return expected result when calling function with (0)', () => {
+    it('should return expected object when calling function with (0)', () => {
       const expected: AmicableNumberObject = { isAmicable: false, pair: null }
       const result = isAmicableNumber(0); expect(result).to.be.deep.equal(expected)
     })
@@ -344,12 +344,12 @@ describe('Test divisorsProductsUtils', () => {
       const result = findAmicableNumbersUnder(0); expect(result).to.be.deep.equal(expected)
     })
 
-    it('should return expected result when calling function with (220)', () => {
+    it('should return expected array when calling function with (220)', () => {
       const expected = [220, 284]
       const result = findAmicableNumbersUnder(221); expect(result).to.be.deep.equal(expected)
     })
 
-    it('should return 20 first amicable numbers when calling function with (66993)', () => {
+    it('should return the first 20 amicable numbers when calling function with (66993)', () => {
       const expected = [
         220, 284, 1184, 1210, 2620, 2924, 5020, 5564, 6232, 6368,
         10744, 10856, 12285, 14595, 17296, 18416, 63020, 76084, 66928, 66992,

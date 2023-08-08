ec140f77385400a82bc2308c5d377c1042d03635
dbtrnl <daniel.bittar@alumni.usp.br>
2023-08-07 22:15:05 -0300

    feat(golang): problems 2 to 4 solved
    also reorganizing folder structure to conform to golang standards

--- golang/cmd/main.go
+++ golang/cmd/main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"fmt"
+	"reflect"
+	"runtime"
+	"strings"
 
-	problems "github.com/dbtrnl/project-euler/golang/pkg/problems/1-10"
+	problems "github.com/dbtrnl/project-euler/golang/internal/problems/1-10"
 )
 
-func main() {
-	answer := problems.Problem1()
-	fmt.Println(answer)
+var problemsSlice = []func()int {
+	problems.Problem1,
+	problems.Problem2,
+	problems.Problem3,
+	problems.Problem4,
 }
+
+func main() {
+	for _, fn := range problemsSlice {
+		result := fn()
+		fnName := strings.Split(runtime.FuncForPC(reflect.ValueOf(fn).Pointer()).Name(), ".")[2]
+		fmt.Printf("Answer of '%s' is: %d\n", fnName, result)
+	}
+}
+
